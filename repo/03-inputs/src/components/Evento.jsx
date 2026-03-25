9291ad4fad8bc1289afda66b310d5d665b93a994
GabrieliDuarte <gabrieliduarte21@gmail.com>
2026-03-25 13:12:25 -0300

    estudos

--- repo/03-inputs/src/components/Evento.jsx
+++ repo/03-inputs/src/components/Evento.jsx
@@ -2,8 +2,15 @@ import {useState} from 'react'
 
 function Evento() {
     const[inputEvento, setInputEvento] = useState('')
-    function handleInput(e){
-        setInputEvento(e.target.value)
+    function handleInput(e){ //event
+        //O que é esse e? Sempre que você clica em algo, digita uma letra ou arrasta o mouse, o navegador cria um
+        //  "pacote de informações" sobre o que acabou de acontecer. Esse pacote diz qual tecla foi apertada, em qual 
+        // coordenada da tela o mouse estava, quem foi clicado, etc.
+
+        setInputEvento(e.target.value) 
+        // 'e' é o evento, target é o "alvo" do evento, ou seja, a tag HTML onde a ação aconteceu, no caso daqui é o input
+        //value: É o valor atual, o texto que está escrito dentro desse input.
+        //setInputEvento salva esse texto na memória do componente.
         console.log(e);
     }
 
@@ -14,10 +21,12 @@ function Evento() {
 
             <input type="text"
                 value={inputEvento}
-                onChange={handleInput}
+                onChange={handleInput} //é um ouvinte de eventos
             />
         </div>
     )
+    //Se você tem um botão e quer saber quando ele foi pressionado ➡️ Usa o onClick.
+    //Se você tem um campo de texto e quer saber quando o conteúdo dele foi alterado ➡️ Usa o onChange.
 }
 
 export default Evento
